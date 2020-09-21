f7350442d5f66d1016a01e5f4264c1f1e9028bbd
Simone Hoffmann <simohoff@gmx.de>
2020-09-21 19:09:31 +0200

    ínitial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const grid = document.querySelector(".grid");
+  const width = 10;
+  let squares = Array.from(document.querySelectorAll(".grid div"));
+  const ScoreDisplay = document.querySelector("#score");
+  const StartBtn = document.querySelector("#start-button");
+
+  console.log(grid);
+  console.log(squares);
+});
